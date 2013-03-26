f30eca3cef147719b48201ab22fe59d034a214bc
Sven Ehrke <sven.ehrke@canoo.com>
2013-03-26 08:58:16 +0100

    added original script content to 'GdtMain'

--- src/main/groovy/org/svenehrke/directorytemplate/tool/GdtMain.groovy
+++ src/main/groovy/org/svenehrke/directorytemplate/tool/GdtMain.groovy
@@ -1,7 +1,100 @@
 package org.svenehrke.directorytemplate.tool
 
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.CloneCommand
+import org.eclipse.jgit.api.ReflogCommand
+
 class GdtMain {
+	String userHome = System.properties['user.home']
+	String gdtHome = System.properties['user.home'] + '/.gdt'
+	String templateDirectory = 'templatedirectory'
+
 	def run(String[] args) {
-		println "hello"
+		if (args.size() < 1) {
+			printUsage()
+			System.exit(1)
+		}
+
+		def command = args[0]
+
+		if (command == 'install') {
+
+			def gitHubMode = args[1] == '-github'
+
+			String ghUserName = 'svene'
+			String ghRepoName = 'dt_java'
+			String remoteUrl
+			String localRepoLocation
+			if (gitHubMode) {
+				if (args.size() < 4) {
+					printUsage()
+					System.exit(1)
+				}
+				ghUserName = args[2]
+				ghRepoName = args[3]
+				remoteUrl = "https://github.com/${ghUserName}/${ghRepoName}.git"
+				localRepoLocation = "${gdtHome}/${ghRepoName}"
+			}
+			else {
+				println 'Non github mode not supported yet'
+				System.exit(0)
+			}
+
+			FileRepositoryBuilder builder = new FileRepositoryBuilder();
+			Repository repository = builder.setWorkTree(new File(gdtHome)).build()
+			Git git = new Git(repository);
+			CloneCommand clone = git.cloneRepository();
+			clone.setBare(false);
+			clone.setCloneAllBranches(true);
+			clone.setDirectory(new File(localRepoLocation)).setURI(remoteUrl);
+			clone.call();
+
+			println "Repository '${remoteUrl}' cloned into local folder '${localRepoLocation}'"
+		}
+		else if (command == 'list') {
+			new File(gdtHome).eachDir { f ->
+				println "${f.name}:"
+				new File("${gdtHome}/${f.name}/templatedirectory").eachDir { dt ->
+					println "  ${dt.name}"
+				}
+			}
+		}
+		else if (command == 'apply') {
+			if (args.size() < 2) {
+				printUsage()
+				System.exit(1)
+			}
+			// todo: dynamically read in installed directory templates. Until then hard coded here:
+			String dtName = args[1]
+		}
+		else {
+			println "unknown command '${command}'"
+		}
+	}
+
+	def printUsage() {
+		println """
+	usage:
+	  groovy gdt2.groovy install -github <user> <repo> <templatedirectory (default='templatedirectory')>
+	  //groovy gdt2.groovy install <repourl> <templatedirectory (default='templatedirectory')>
+	  groovy gdt2.groovy list
+	  groovy gdt2.groovy apply <directorytemplate>
+
+	examples:
+	  groovy gdt2.groovy install -github svene dt_java
+	  //groovy gdt2.groovy https://github.com/svene/dt_java.git
+	  groovy gdt2.groovy apply simplejava
+	"""
 	}
 }
+
+//----------------------------------------------------
+/*
+git.checkout().setName('eb36bddf2fff3d74d2bafdc510481422408beff2').call()
+
+ReflogCommand rlc = new ReflogCommand()
+rlc.setRef('d1c7c7877430ee122bbc14241f920c1fea34b27e')
+rlc.call()
+*/
