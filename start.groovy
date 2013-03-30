47442b95a9cc72e5ea79eddd83796bf0d0adcfd9
Sven Ehrke <sven.ehrke@canoo.com>
2013-03-30 10:28:35 +0100

    updated start script

--- start.groovy
+++ start.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='sven-github', root='http://svene.github.com/maven2/releases')
-@Grab(group='org.svenehrke', module='directory_template', version='0.0.6-SNAPSHOT')
+@Grab(group='org.svenehrke', module='directory_template', version='0.0.7')
 import org.svenehrke.directorytemplate.tool.GdtMain
 
 new GdtMain().run(args)
